f6f5bd5db3dc4d781e12bb719fdc148f3016a453
JI00000 <parkjy990820@sookmyung.ac.kr>
2021-11-25 00:42:16 +0900

    취향분석 산책로 프론트 (최초 사용자 ver)

--- app/src/main/java/com/cookandroid/onjung/PreferenceTestActivity.java
+++ app/src/main/java/com/cookandroid/onjung/PreferenceTestActivity.java
@@ -1,6 +1,9 @@
 package com.cookandroid.onjung;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,23 @@ public class PreferenceTestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preference_test);
+
+        CheckBox mountain, park, lake, forest, river;
+        CheckBox crowded, quiet, pet, scenery, show, statue, exercise, walk;
+
+        mountain = (CheckBox) findViewById(R.id.mountain);
+        park = (CheckBox) findViewById(R.id.park);
+        lake = (CheckBox) findViewById(R.id.lake);
+        forest = (CheckBox) findViewById(R.id.forest);
+        river= (CheckBox) findViewById(R.id.river);
+
+        crowded = (CheckBox) findViewById(R.id.crowded);
+        quiet = (CheckBox) findViewById(R.id.quiet);
+        pet = (CheckBox) findViewById(R.id.pet);
+        scenery = (CheckBox) findViewById(R.id.scenery);
+        show= (CheckBox) findViewById(R.id.show);
+        statue = (CheckBox) findViewById(R.id.statue);
+        exercise = (CheckBox) findViewById(R.id.exercise);
+        walk = (CheckBox) findViewById(R.id.walk);
     }
 }
